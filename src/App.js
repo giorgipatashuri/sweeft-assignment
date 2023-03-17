23eac1d1f35141f4617d2f8daf9a0572441d3cbc
unknown <patashuri2003@gmail.com>
2023-03-17 23:03:59 +0400

    added basic routing and fixed ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import DetailsPage from './pages/DetailsPage/DetailsPage';
+
 import Home from './pages/Home/Home';
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/user/:id' element={<DetailsPage />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,15 +1,18 @@
+import { Link } from 'react-router-dom';
 import './Card.scss';
 
 const Card = ({ data }) => {
   return (
     <div className='cardItem'>
-      <div>
-        <img src={data.imageUrl + `?v=${data.id}`} alt='' />
-      </div>
-      <div>
-        <h3>{`${data.prefix} ${data.name} ${data.lastName}`}</h3>
-        <p>{data.title}</p>
-      </div>
+      <Link to={`/user/${data.id}`}>
+        <div>
+          <img src={data.imageUrl + `?v=${data.id}`} alt='' />
+        </div>
+        <div>
+          <h3>{`${data.prefix} ${data.name} ${data.lastName}`}</h3>
+          <p>{data.title}</p>
+        </div>
+      </Link>
     </div>
   );
 };
